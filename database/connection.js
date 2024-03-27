90460f6f803f59a6b5ef05e6a4c52d03b9b64515
AkshanshChauhan17 <akshanshchauhan47@gmail.com>
2024-03-27 15:43:48 +0530

    connection debug false

--- database/connection.js
+++ database/connection.js
@@ -3,7 +3,8 @@ import mysql from "mysql";
 var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    database: "gsmith"
+    database: "gsmith",
+    debug: false
 });
 
 export default connection;
