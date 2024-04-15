d565a12949a37cf0e6cd477c6bfc05e5213d7b63
AkshanshChauhan17 <akshanshchauhan47@gmail.com>
2024-04-16 04:59:06 +0530

    admin roi

--- routes/admin.js
+++ routes/admin.js
@@ -4,26 +4,65 @@ import express from 'express';
 const adminRouter = express.Router();
 
 adminRouter.get('/all_users', (req, res) => {
-    executeQuery("SELECT user_id, email, meta FROM user WHERE 1", [])
+    executeQuery("SELECT user_id AS id, is_admin, email, meta FROM user WHERE 1", [])
         .then((result) => {
             return res.json(result.map((d) => {
-                d.image = JSON.parse(d.meta).profile_photo.small
-                d.meta = []
-                return d
+                d.user_dp = JSON.parse(d.meta).profile_photo.small;
+                const meta = JSON.parse(d.meta);
+                meta.profile_photo = undefined;
+                d.meta = meta;
+                return d;
             }));
         }).catch((error) => {
             return res.json(error);
         });
 });
 
-adminRouter.get('/one_user/:user_id', (req, res) => {
+adminRouter.get('/count_users', (req, res) => {
+    executeQuery("SELECT count(*) AS users FROM user WHERE 1", [])
+        .then((result) => {
+            return res.json(...result);
+        }).catch((error) => {
+            return res.json(error);
+        });
+});
+
+adminRouter.get('/count_orders', (req, res) => {
+    executeQuery("SELECT count(*) AS orders FROM all_order WHERE 1", [])
+        .then((result) => {
+            return res.json(...result);
+        }).catch((error) => {
+            return res.json(error);
+        });
+});
+
+adminRouter.get('/count_reviews', (req, res) => {
+    executeQuery("SELECT count(*) AS reviews FROM product_rating WHERE 1", [])
+        .then((result) => {
+            return res.json(...result);
+        }).catch((error) => {
+            return res.json(error);
+        });
+});
+
+adminRouter.get('/view_user/:user_id', (req, res) => {
     const { user_id } = req.params;
     executeQuery("SELECT user_id, email, meta FROM user WHERE user_id=?", [user_id])
         .then((result) => {
+            return res.json(...result.map((d) => {
+                d.meta = JSON.parse(d.meta);
+                return d;
+            }));
+        }).catch((error) => {
+            return res.json(error);
+        });
+});
+
+adminRouter.get('/revenue_graph_data', (req, res) => {
+    executeQuery("SELECT timestamp as name, total_cost as Total FROM all_order WHERE is_conform=1", [])
+        .then((result) => {
             return res.json(result.map((d) => {
-                d.user_dp = JSON.parse(d.meta).profile_photo.small
-                d.user_meta = undefined
-                d.meta = undefined
+                d.name = new Date(d.name).toLocaleDateString()
                 return d
             }));
         }).catch((error) => {
@@ -31,8 +70,31 @@ adminRouter.get('/one_user/:user_id', (req, res) => {
         });
 });
 
-adminRouter.get('all_products', (req, res) => {
-    executeQuery("SELECT * FROM products WHERE 1", [])
+adminRouter.get('/user/revenue_graph_data/:user_id', (req, res) => {
+    const { user_id } = req.params;
+    executeQuery("SELECT timestamp as name, total_cost as Total FROM all_order WHERE is_conform=1 AND user_id=?", [user_id])
+        .then((result) => {
+            return res.json(result.map((d) => {
+                d.name = new Date(d.name).toLocaleDateString()
+                return d
+            }));
+        }).catch((error) => {
+            return res.json(error);
+        });
+});
+
+adminRouter.get('/earning', (req, res) => {
+    executeQuery("SELECT SUM(total_cost) AS earning FROM all_order WHERE is_conform=1", [])
+        .then((result) => {
+            return res.json(...result);
+        }).catch((error) => {
+            return res.json(error);
+        });
+});
+
+adminRouter.delete('/delete_user/:user_id', (req, res) => {
+    const { user_id } = req.params;
+    executeQuery("DELETE FROM user WHERE user_id=?", [user_id])
         .then((result) => {
             return res.json(result);
         }).catch((error) => {
@@ -40,8 +102,33 @@ adminRouter.get('all_products', (req, res) => {
         });
 });
 
-adminRouter.get('all_orders', (req, res) => {
-    executeQuery("SELECT * FROM product_order WHERE 1", [])
+adminRouter.patch('/action_user/:user_id', (req, res) => {
+    const { user_id } = req.params;
+    const { action, value } = req.body;
+    switch (action) {
+        case "admin":
+            executeQuery("UPDATE INTO user SET is_admin=? WHERE user_id=?", [value, user_id])
+                .then((result) => {
+                    return res.json(result);
+                }).catch((error) => {
+                    return res.json(error);
+                });
+            break;
+        case "block":
+            executeQuery("UPDATE INTO user SET is_block=? WHERE user_id=?", [value, user_id])
+                .then((result) => {
+                    return res.json(result);
+                }).catch((error) => {
+                    return res.json(error);
+                });
+            break;
+        default:
+            res.json({ status: false, message: "Please select valid action!!!" });
+    };
+});
+
+adminRouter.get('/all_products', (req, res) => {
+    executeQuery("SELECT * FROM product WHERE 1", [])
         .then((result) => {
             return res.json(result);
         }).catch((error) => {
@@ -49,7 +136,20 @@ adminRouter.get('all_orders', (req, res) => {
         });
 });
 
-adminRouter.get('all_reviews', (req, res) => {
+adminRouter.get('/all_orders', (req, res) => {
+    executeQuery("SELECT * FROM all_order ORDER BY timestamp DESC LIMIT 10", [])
+        .then((result) => {
+            return res.json(result.map((r) => {
+                r.product_list = JSON.parse(r.product_list);
+                r.user_meta = JSON.parse(r.user_meta);
+                return r;
+            }));
+        }).catch((error) => {
+            return res.json(error);
+        });
+});
+
+adminRouter.get('/all_reviews', (req, res) => {
     executeQuery("SELECT * FROM product_review WHERE 1", [])
         .then((result) => {
             return res.json(result);
