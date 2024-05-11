fc3ed25871d036e55b55354b07a37adb463028c4
AkshanshChauhan17 <akshanshchauhan47@gmail.com>
2024-05-12 03:36:41 +0530

    dsf

--- index.js
+++ index.js
@@ -14,21 +14,18 @@ const adminRouter = require('./routes/admin.js');
 
 var api = express();
 
-const __dirname = dirname(fileURLToPath(
-    import.meta.url));
-
 api.use('/static', express.static(path.join(__dirname, 'upload')));
 
 api.use(express.urlencoded({ extended: true }));
 api.use(express.json({ limit: "500mb" }));
 api.use(cors());
 
-api.use('/user', userRouter);
-api.use('/navigation', navigationRouter);
-api.use('/product', productRouter);
-api.use('/media', mediaRouter);
-api.use('/review', reviewRouter);
-api.use('/order', orderRouter);
-api.use('/admin', adminRouter);
+api.use('/api/user', userRouter);
+api.use('/api/navigation', navigationRouter);
+api.use('/api/product', productRouter);
+api.use('/api/media', mediaRouter);
+api.use('/api/review', reviewRouter);
+api.use('/api/order', orderRouter);
+api.use('/api/admin', adminRouter);
 
 api.listen();
